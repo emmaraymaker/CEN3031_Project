bd77e6a19c02a9022136a183ac21abf3b72eeac0
rrosalinda <101357257+rrosalinda@users.noreply.github.com>
2023-02-08 21:32:01 -0500

    update files, testing

--- frontend-actors-housing/src/app/app.component.spec.ts
+++ frontend-actors-housing/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'practice-project'`, () => {
+  it(`should have as title 'actors-housing-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('practice-project');
+    expect(app.title).toEqual('actors-housing-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('practice-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('actors-housing-project app is running!');
   });
 });
--- frontend-actors-housing/src/app/app.module.ts
+++ frontend-actors-housing/src/app/app.module.ts
@@ -1,36 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
-import { MaterialModule } from './material/material.module';
 import { RegistrationComponent } from './registration/registration.component';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 import { UserComponent } from './user/user.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    UserComponent,
+    UserComponent
   ],
   imports: [
-    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    ReactiveFormsModule,
     RouterModule.forRoot([
       {path: 'app', component: AppComponent},
       {path: 'login', component: LoginComponent},
       {path: 'registration', component: RegistrationComponent},
       {path: 'user', component: UserComponent}
     ]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
