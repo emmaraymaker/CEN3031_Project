235095b195d1edbc0822da697fcb361b84558f33
rrosalinda <101357257+rrosalinda@users.noreply.github.com>
2023-03-01 10:37:34 -0500

    frontend files before testing

--- actors-housing/src/app/app.module.ts
+++ actors-housing/src/app/app.module.ts
@@ -1,33 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
+import { MaterialModule } from './material/material.module';
 import { RegistrationComponent } from './registration/registration.component';
-import { UserComponent } from './user/user.component';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    UserComponent
+    UserComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {path: 'app', component: AppComponent},
       {path: 'login', component: LoginComponent},
       {path: 'registration', component: RegistrationComponent},
       {path: 'user', component: UserComponent}
     ]),
-    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- actors-housing/src/app/registration/registration.component.ts
+++ actors-housing/src/app/registration/registration.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-
+ 
 }
+
