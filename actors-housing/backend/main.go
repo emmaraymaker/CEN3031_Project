899c6d94b34d20cb9819c022ba7074e61bb1d808
CGator <69414105+CGator@users.noreply.github.com>
2023-03-29 20:47:05 -0400

    Update main.go

--- actors-housing/backend/main.go
+++ actors-housing/backend/main.go
@@ -1,40 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
-	backen "test/Structs"
+	backend "test/Structs"
 )
+
+var memL backend.MemberList
 
 func main() {
 
 	r := mux.NewRouter()
 
-	//r.HandleFunc("/hello-world", helloWorld)
+	r.HandleFunc("/user/reg", registerUser).Methods("GET")
+	r.HandleFunc("/user/get", getUser).Methods("GET")
+	r.HandleFunc("/user/update", updateUser).Methods("POST")
+	r.HandleFunc("/user/search", searchUser).Methods("POST")
+	r.HandleFunc("/listing/set", setListing).Methods("PUT")
+	r.HandleFunc("/listing/get", getListing).Methods("DELETE")
+	r.HandleFunc("/listing/update", updateListing).Methods("DELETE")
+	r.HandleFunc("/listing/search", searchListing).Methods("DELETE")
 
-	http.Handle("/", r)
+	http.ListenAndServe(":8000", r)
 
 	srv := &http.Server{
 		Handler: r,
 		Addr:    ":" + os.Getenv("PORT"),
-	}
-
-	var memL backen.MemberList
-
-	memL.AddMember("1", "Test, t", "888-888-8888", "test@test.com", "password", false, false)
-	memL.AddMember("2", "Test2, t", "888-888-8888", "test@test.com", "password", false, false)
-	memL.AddMember("3", "Test3, t", "888-888-8888", "test@test.com", "password", false, false)
-
-	var name, _ = memL.SearchMember("2")
-	fmt.Println(name)
-
-	memL.RemoveMember("2")
-
-	for i := 0; i < 2; i++ {
-		fmt.Println(memL.Ml[i].Name)
 	}
 
 	log.Fatal(srv.ListenAndServe())
@@ -42,45 +36,87 @@ func main() {
 	// Test set for pre-front-end integration
 
 	/*
+		var memL backen.MemberList
 
-		var memL MemberList
-		var lisL ListingSet
+			memL.AddMember("1", "Test, t", "888-888-8888", "test@test.com", "password", false, false)
+			memL.AddMember("2", "Test2, t", "888-888-8888", "test@test.com", "password", false, false)
+			memL.AddMember("3", "Test3, t", "888-888-8888", "test@test.com", "password", false, false)
 
-		var uid string
-		var name string
-		var phone string
-		var email string
-		fmt.Println("Hello, please enter your Union Id:")
-		fmt.Scanln(&uid)
-		fmt.Println("Please enter your name:")
-		fmt.Scanln(&name)
-		fmt.Println("Please enter your Phone number:")
-		fmt.Scanln(&phone)
-		fmt.Println("Please enter your Email address:")
-		fmt.Scanln(&email)
-		memL.addMember(uid, name, phone, email, isL, isT)
+			var name, _ = memL.SearchMember("2")
+			fmt.Println(name)
 
-		for {
-			fmt.Println("Please select a menu option:\n1: Rent\n2:List")
+			memL.RemoveMember("2")
 
-			var option string
-			fmt.Scanln(&option)
-
-			var location string
-			var price int
-			if option == "1" {
-				fmt.Println("displaying listings:")
-				fmt.Println(lisL.ls[0].location)
-			} else if option == "2" {
-
-				fmt.Println("Please enter the Listing's Location:")
-				fmt.Scanln(&location)
-				fmt.Println("Please enter the Listing's Price:")
-				fmt.Scanln(&price)
-				lisL.addListing(location, price)
-			} else {
-				break
+			for i := 0; i < 2; i++ {
+				fmt.Println(memL.Ml[i].Name)
 			}
-		}
+			var memL MemberList
+			var lisL ListingSet
+
+			var uid string
+			var name string
+			var phone string
+			var email string
+			fmt.Println("Hello, please enter your Union Id:")
+			fmt.Scanln(&uid)
+			fmt.Println("Please enter your name:")
+			fmt.Scanln(&name)
+			fmt.Println("Please enter your Phone number:")
+			fmt.Scanln(&phone)
+			fmt.Println("Please enter your Email address:")
+			fmt.Scanln(&email)
+			memL.addMember(uid, name, phone, email, isL, isT)
+
+			for {
+				fmt.Println("Please select a menu option:\n1: Rent\n2:List")
+
+				var option string
+				fmt.Scanln(&option)
+
+				var location string
+				var price int
+				if option == "1" {
+					fmt.Println("displaying listings:")
+					fmt.Println(lisL.ls[0].location)
+				} else if option == "2" {
+
+					fmt.Println("Please enter the Listing's Location:")
+					fmt.Scanln(&location)
+					fmt.Println("Please enter the Listing's Price:")
+					fmt.Scanln(&price)
+					lisL.addListing(location, price)
+				} else {
+					break
+				}
+			}
 	*/
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	var usr backend.Member
+	_ = json.NewDecoder(r.Body).Decode(&usr)
+	memL.Ml = append(memL.Ml, usr)
+	//json.NewEncoder(w).Encode(memL)
+}
+func getUser(w http.ResponseWriter, r *http.Request) {
+
+}
+func updateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+func searchUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func setListing(w http.ResponseWriter, r *http.Request) {
+
+}
+func getListing(w http.ResponseWriter, r *http.Request) {
+
+}
+func updateListing(w http.ResponseWriter, r *http.Request) {
+
+}
+func searchListing(w http.ResponseWriter, r *http.Request) {
+
 }
