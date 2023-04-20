469c805c2e1a1556e18edfc2430156b310569e2c
emmaraymaker <emmaraymaker@gmail.com>
2023-04-19 22:27:39 -0400

    listings.component.spec.ts
    This test creates an instance of the ListingsComponent, sets some input values, calls the addListing() function, and checks if a new listing is added to the Listings array with the correct values. Note that this test does not require any Angular testing utilities since we are not testing the component's template or any Angular-specific functionality.

--- actors-housing/frontend/src/app/listings/listings.component.spec.ts
+++ actors-housing/frontend/src/app/listings/listings.component.spec.ts
@@ -1,23 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListingsComponent } from './listings.component';
 
 describe('ListingsComponent', () => {
   let component: ListingsComponent;
-  let fixture: ComponentFixture<ListingsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ListingsComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ListingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new ListingsComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should add a listing to the Listings array', () => {
+    const initialLength = component.Listings.length;
+    component.state = 'California';
+    component.city = 'Los Angeles';
+    component.street = '123 Main St';
+    component.unit = 'Apt 1';
+    component.zipcode = '90001';
+    component.description = 'A cozy apartment';
+
+    component.addListing();
+
+    expect(component.Listings.length).toEqual(initialLength + 1);
+    expect(component.Listings[initialLength]).toEqual({
+      state: 'California',
+      city: 'Los Angeles',
+      street: '123 Main St',
+      unit: 'Apt 1',
+      zipcode: '90001',
+      description: 'A cozy apartment'
+    });
   });
 });
