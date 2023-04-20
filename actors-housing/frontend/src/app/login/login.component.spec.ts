661687087d560c56f175debe864c72694c04d75a
emmaraymaker <emmaraymaker@gmail.com>
2023-04-19 22:28:33 -0400

    login.component.spec.ts
    This test creates an instance of the LoginComponent, sets the email property, calls the onSubmit() function, and checks if the email property is still set to the same value after the function is called. Note that this test does not require any Angular testing utilities since we are not testing the component's template or any Angular-specific functionality.

--- actors-housing/frontend/src/app/login/login.component.spec.ts
+++ actors-housing/frontend/src/app/login/login.component.spec.ts
@@ -1,23 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new LoginComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set email on submit', () => {
+    const email = 'test@example.com';
+    component.email = email;
+
+    component.onSubmit();
+
+    expect(component.email).toEqual(email);
   });
 });
